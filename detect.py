6cf220a31f0c71301920190e931f3501f4b65054
enes3774 <77508537+enes3774@users.noreply.github.com>
2022-09-09 09:48:01 +0300

    Update detect.py
    Signed-off-by: enes3774 <77508537+enes3774@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -58,7 +58,6 @@ def run(
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results
         save_txt=True,  # save results to *.txt
         save_conf=False,  # save confidences in --save-txt labels
