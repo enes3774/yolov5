ffcb12b41e7f371c8522a7fd9f65740f9b462ca7
enes3774 <77508537+enes3774@users.noreply.github.com>
2022-09-10 14:40:19 +0300

    Update detect.py
    Signed-off-by: enes3774 <77508537+enes3774@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -117,7 +117,7 @@ def run(
         print(len(pred))
         # NMS
         with dt[2]:
-            pred = non_max_suppression(pred, 0, iou_thres, classes, agnostic_nms, max_det=max_det)
+            pred = non_max_suppression(pred, 0.1, iou_thres, classes, agnostic_nms, max_det=max_det)
 
         # Second-stage classifier (optional)
         # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
@@ -139,13 +139,16 @@ def run(
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
                 min_dist=None
                 best_xyxy,best_conf=det[0][0:4],float(det[0][4])
+                print("best:"+str(best_conf))
                 if coord==None:
                     return best_xyxy,best_conf
+                
                 for *xyxy, conf, cls in det:
 
                         x0n,y0n,x1n,y1n=xyxy
                         if coord!=None:
                             dist=math.sqrt((x0-x0n)**2+(y0-y0n)**2)
+                            print(dist)
                             if dist>20:
                                 continue
                             else:
@@ -157,18 +160,23 @@ def run(
                         #label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
             else:
                 return None
-            if min_conf>0.75:
-                return min_xyxy,min_conf
-            if best_conf>0.75:
-                return best_xyxy,best_conf
             if min_dist==None and best_conf<0.5:
                 return None
             if min_dist==None and best_conf>=0.5:
                 return best_xyxy,best_conf
-            if best_conf<0.25 and min_conf<0.25:
-                return None
             if (best_conf==min_conf):
                 return min_xyxy,min_conf
+            if min_conf>0.75:
+                return min_xyxy,min_conf
+            if best_conf>0.75:
+                return best_xyxy,best_conf
+            
+            if best_conf<0.25:
+                if min_conf>0.1:
+                    return min_xyxy,min_conf
+                else:
+                    return None
+
             x0b,y0b,x1b,y1b=best_xyxy
             dist=math.sqrt((x0-int(x0b))**2+(y0-int(y0b))**2)
             if min_conf>(best_conf-dist*0.002):
